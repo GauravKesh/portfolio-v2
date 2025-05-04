7e9a8d6a39a2e48eb6dc16b7ed4f7f81bcb1e7bb
GauravKesh <gkrcoder@gmail.com>
2025-05-05 02:39:31 +0530

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://gkrcoder.vercel.app',  // Replace with your website URL
+    generateRobotsTxt: true,  // Generates robots.txt file automatically
+    changefreq: 'daily',  // Change frequency, adjust as needed
+    priority: 0.7,  // Default priority
+}
