6fc3c076802036df07bf4ab434cffb574cd2b9ed
GauravKesh <gkrcoder@gmail.com>
2025-05-07 02:36:12 +0530

    ui change in conatct

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -614,8 +614,8 @@ export default function ContactSection() {
                 </p>
                 <div className="bg-primary/10 text-primary font-medium px-4 py-2 rounded-md inline-flex items-center">
                   <span className="relative flex h-3 w-3 mr-2">
-                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75"></span>
-                    <span className="relative inline-flex rounded-full h-3 w-3 bg-primary"></span>
+                  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-600 opacity-75 "></span>
+                  <span className="relative inline-flex rounded-full h-3 w-3 bg-green-800"></span>
                   </span>
                   Open to opportunities
                 </div>
